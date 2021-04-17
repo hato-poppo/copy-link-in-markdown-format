a935d06d4e52c0612c6e3eeb101a7a2baea1f891
hato-poppo <ysng1224@gmail.com>
2021-04-18 08:31:50 +0900

    Code Refactoring

--- event.js
+++ event.js
@@ -11,14 +11,18 @@
 
   // メニューをクリック時に実行
   chrome.contextMenus.onClicked.addListener(() => {
-
-    chrome.tabs.query({'active': true, 'lastFocusedWindow': true}, tabs => {
-      const link = `[${tabs[0].title}](${tabs[0].url})`;
-      copyToClipboard(link);
-    });
-
+    chrome.tabs.query({currentWindow: true, active: true},
+      (tabs) => generateLinkText(tabs).then(copyToClipboard).catch(onError));
   });
 
+  const generateLinkText = (tabs) => {
+    const tab = tabs[0]; // Current Window && Active は1つしか存在しないので、最初の要素だけ取得でOK
+    return new Promise((resolve) => {
+      resolve(`[${tab.title}](${tab.url})`);
+    });
+  }
+
+  // TODO: もっとイケてる書き方があるはず。
   const copyToClipboard = (text) => {
       var tmpArea = document.createElement("textarea");
       tmpArea.textContent = text;
@@ -34,5 +38,9 @@
       document.execCommand('copy');
       // 追加テキストエリアを削除
       bodyElm.removeChild(tmpArea);
-  } 
+  }
+
+  const onError = (e) => {
+    console.log(`[copy-link-in-markdown-format]【ERROR】: ${e}`);
+  }
 }
